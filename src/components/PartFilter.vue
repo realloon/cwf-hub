3295fa8e04dfada68f58d7c65934d5900aff2f8c
sincerajn <sincerajn@outlook.com>
2026-06-09 09:54:15 +0800

    feat: add Vue integration and PartFilter component

---
+++ src/components/PartFilter.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface Props {
+  parts: { id: string; label: string }[]
+}
+
+defineProps<Props>()
+
+const selected = ref<string[]>([])
+</script>
+
+<template>
+  <section>
+    <pre>{{ selected }}</pre>
+    <label v-for="{ id, label } in parts">
+      <span> {{ label }}</span>
+      <input type="checkbox" :name="id" :value="id" v-model="selected" />
+    </label>
+  </section>
+</template>
+
+<style scoped></style>
